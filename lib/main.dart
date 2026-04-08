756ae162fc2e2bf7c6b200ed3bc8294c8c4ab861
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2026-04-08 10:57:20 +0300

    initializing firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'filter_sheet.dart';
 import 'screens/auth_flow.dart';
@@ -6,6 +7,8 @@ import 'widgets/product_card.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const GroceryApp());
 }
 
